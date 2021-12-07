f620d3d02cf41b71811218468084f4d775a6dbd7
Daniel Kiss <littlesnorrboy@gmail.com>
2021-12-07 18:48:28 +0100

    Prefix unused variable with _

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let mut buffer = String::with_capacity(1024);
-    while let Ok(size) = std::io::stdin().read_line(&mut buffer) {
+    while let Ok(_size) = std::io::stdin().read_line(&mut buffer) {
         let line: &str = buffer.as_str().trim_end();
         if line.len() == 0 {
             break;
